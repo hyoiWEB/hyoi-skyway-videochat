f1e36debca3d864d8b26a016cf569e0534a5b185
Miguel <shota@miurashoutanoMacBook-Pro.local>
2020-02-21 20:48:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,83 @@
+/*ナビゲーション----------------------------------------------------------------*/
 /* eslint-disable require-jsdoc */
 'use strict'
 
 $(function() {
+
+  var
+	  winW = $(window).width(),
+		winH = $(window).height(),
+		nav = $('#mainnav ul a'),
+		curPos = $(this).scrollTop();
+
+	if (winW < 880){
+		var headerH =0;
+	}
+	else{
+		var headerH =63;
+	}
+
+	$(nav).on('click', function(){
+		nav.removeClass('active');
+  	var $el = $(this),
+		id = $el.attr('href');
+ 		$('html, body').animate({
+   		scrollTop: $(id).offset().top - headerH
+ 		}, 500);
+		$(this).addClass('active');
+		if (winW < 880){
+			$('#menuWrap').next().slideToggle();
+			$('#menuBtn').removeClass('close');
+		}
+ 		return false;
+	});
+
+	var timer = false;
+	$(window).bind('load resize',function(){
+		if (timer !== false){clearTimeout(timer);}
+		timer = setTimeout(function(){
+			var
+				w = $(window).innerWidth(),
+				bg = $('.bg'),
+				bgH = bg.height();
+
+			if(w > 800){
+				$(function(){
+			  	$(".vMid").css('height', bgH);
+				});
+			}
+			else{
+				$(function(){
+			  	$(".vMid").css({'height':'auto','padding':'50px 20px'});
+				});
+			}
+		});
+	});
+
+	$('.panel').hide();
+	$('#menuWrap').toggle(function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').toggleClass('close');
+	},
+	function(){
+		$(this).next().slideToggle();
+		$('#menuBtn').removeClass('close');
+	});
+
+	$(window).on('scroll', function(){
+		var curPos = $(this).scrollTop();
+		if(curPos > 80){
+			$('#mainnav').addClass('changeNav');
+		}
+		else{
+			$('#mainnav').removeClass('changeNav');
+		}
+	});
+
+/*----------------------------------------------------------------------------*/
+
+/*ビデオチャット----------------------------------------------------------------*/
+
   // Peer object
   const peer = new Peer({
     key:   "bc3292a3-35bd-4289-ac50-359c8100377c",
@@ -183,3 +259,5 @@ $(function() {
     $('#step3').show();
   }
 });
+
+/*----------------------------------------------------------------------------*/
