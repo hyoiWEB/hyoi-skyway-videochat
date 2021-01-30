4841b8320d1d94f1e3be9fa2b363c8c331ee4e36
Miguel <shota.merry.go.round@gmail.com>
2021-01-31 03:27:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const Peer = window.Peer;
   const callTrigger = document.getElementById('js-call-trigger');
   const closeTrigger = document.getElementById('js-close-trigger');
   const remoteVideo = document.getElementById('js-remote-stream');
-  const remoteId = "maid-shokan";
+  const remoteId = document.getElementById('js-remote-id');
   const meta = document.getElementById('js-meta');
   const sdkSrc = document.querySelector('script[src*=skyway]');
 
@@ -38,8 +38,8 @@ const Peer = window.Peer;
       return;
     }
 
-    const mediaConnection = peer.call(remoteId, localStream);
-    const dataConnection = peer.connect(remoteId);
+    const mediaConnection = peer.call(remoteId.value, localStream);
+    const dataConnection = peer.connect(remoteId.value);
 
     mediaConnection.on('stream', async stream => {
       // Render remote stream for caller
